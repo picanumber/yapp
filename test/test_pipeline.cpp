726f54d1b1bf07989357173967689dc93ff94326
picanumber <picanumber@gmail.com>
2022-06-23 23:11:45 +0200

    Add required tests

--- test/test_pipeline.cpp
+++ test/test_pipeline.cpp
@@ -2,8 +2,7 @@
 
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(TestPipeline, BasicAssertions)
 {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
---
+++ test/test_stage.cpp
@@ -0,0 +1,12 @@
+#include "yap/pipeline.h"
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(TestStage, BasicAssertions)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
