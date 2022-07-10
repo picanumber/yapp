7208559cfbec516bcf5248555a46dc691dc5b979
picanumber <picanumber@gmail.com>
2022-07-10 22:14:07 +0200

    Fix build and extend README

--- include/yap/topology.h
+++ include/yap/topology.h
@@ -12,6 +12,14 @@ namespace yap
 template <class T> struct Filtered
 {
     std::optional<T> data;
+
+    Filtered() = default;
+    explicit Filtered(T &&data) : data(std::move(data))
+    {
+    }
+    explicit Filtered(std::optional<T> &&data) : data(std::move(data))
+    {
+    }
 };
 
 namespace detail
@@ -25,7 +33,7 @@ template <class T> constexpr bool kIsFiltered<Filtered<T>> = true;
 template <class F> auto Filter(F &&operation)
 {
     return [op = std::forward<F>(operation)](auto &&...args) {
-        return Filtered{std::invoke(op, std::forward<decltype(args)>(args)...)};
+        return Filtered(std::invoke(op, std::forward<decltype(args)>(args)...));
     };
 }
 
